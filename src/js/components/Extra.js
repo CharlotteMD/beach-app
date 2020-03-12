7366c0240156d63c651e91b5a2f3068dba48578a
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2020-03-12 15:41:22 +0000

    Revert "Delete obsolete files"
    This reverts commit 2b4fb8075849bd9f31d007bb9662df4c0f3f1a1d.

---
+++ src/js/components/Extra.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Extra = () => {
+  return (
+    <div>
+      <h4>Stretch goals include:</h4>
+      <ul>
+        <li>Some kind of dashboard</li>
+        <li>Login - probably via cognito</li>
+        <li>Some more bleeding edge react features</li>
+        <li>Axios</li>
+        <li>Typescript</li>
+        <li>Tests</li>
+        <li>React Router</li>
+      </ul>
+    </div>
+  )
+}
